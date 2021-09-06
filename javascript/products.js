2477b5151ff6cc13f734fa737215881ea5170662
Naykayvd <vandiemennahum@gmail.com>
2021-09-06 15:55:57 +0200

    first commit

--- javascript/products.js
+++ javascript/products.js
@@ -14,8 +14,16 @@ fetch("https://final-eomp.herokuapp.com/view-records/")
             <p>Price: R${item[3]}</p>
             <p>Genre: ${item[4]}</p>
             <p>Year: ${item[5]}</p>
-            <p>Description: ${item[6]}</p>
-            <button>Add to cart<i class="fas fa-cart-plus"></i></button>`
+            <p class="description">Description: ${item[6]}</p>
+            <label for="Quantity">Quantity:</label>
+            <input class="added-${item[0]}"
+                name="Quantity"
+                type="number"
+                min="1"
+                max="40"
+                value="1"
+            />
+            <button id="submit" onclick="addedInCart(${item[0]})">Add to cart<i class="fas fa-cart-plus"></i></button>`
         });
     })
 })
@@ -23,4 +31,41 @@ fetch("https://final-eomp.herokuapp.com/view-records/")
 function createBox(index) {
     let records_containers = document.querySelector(".records-container");
     records_containers.innerHTML += `<div class="item-box${index}"></div>`
+}
+
+// function addedRec(id) {
+//     let added_records = document.querySelector(".cart");
+//     added_records.innerHTML += `<div class="displayed_records">${id}</div>`
+// }
+
+function showCart() {
+    // addedInCart();
+    let cart = document.querySelector(".user-cart");
+    cart.classList.toggle("show");
+}
+
+function addedInCart(id) {
+    let quantity = document.querySelector(`.added-${id}`);
+    console.log(quantity.value);
+    let cart = document.querySelector(".user-cart");
+    fetch("https://final-eomp.herokuapp.com/view-records/")
+    .then((request) => {
+        request.json().then((obj) => {
+            items = obj.data;
+            items.forEach((item) => {
+                if (item[0] == id) {
+                    console.log(item[0])
+                    cart.innerHTML += `
+                    <div class="item">
+                        <p>Record No. ${item[0]}</p>
+                        <h2>Artist: ${item[1]}</h2>
+                        <h2>Album: ${item[2]}</h2>
+                        <p>Price: R${item[3]}</p>
+                        <p>Quantity: ${quantity.value}</p>
+                    </div>
+                    `
+                }
+            })
+        })
+    });
 }
